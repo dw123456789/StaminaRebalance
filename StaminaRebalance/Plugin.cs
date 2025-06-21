ef60f26529013517a01a24a672e64ed24d62c517
dw123456789 <parkourfaith08@gmail.com>
2025-06-21 18:59:12 -0500

    Inital

---
+++ StaminaRebalance/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace StaminaRebalance
+{
+    [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static new ManualLogSource Logger;
+
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+            Harmony harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+            harmony.PatchAll();
+        }
+    }
+    [HarmonyPatch(typeof(CharacterMovement), "Start")]
+    class Patch
+    {
+        static void Postfix(CharacterMovement __instance)
+        {
+            __instance.jumpStaminaUsageSprinting = 0.05f;
+            __instance.sprintStaminaUsage = 0f;
+        }
+    }
+}
---
+++ StaminaRebalance/obj/Debug/net40/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace StaminaRebalance
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "StaminaRebalance";
+        public const string PLUGIN_NAME = "My first plugin";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
